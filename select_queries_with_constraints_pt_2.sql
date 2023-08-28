a13a7a6c748f4fcaa09d580c6a25c22d5ed28312
symonk <jackofspaces@gmail.com>
2023-08-28 14:28:23 +0100

    `select queries with constraints pt 2`

---
+++ select_queries_with_constraints_pt_2.sql
@@ -0,0 +1,11 @@
+/* Exercise 1 */
+SELECT * From Movies WHERE Title LIKE 'Toy Story%'
+
+/* Exercise 2 */
+SELECT * FROM Movies Where Director = "John Lasseter";
+
+/* Exercise 3 */
+SELECT * FROM Movies Where Director != "John Lasseter";
+
+/* Exercise 4 */
+SELECT * FROM Movies WHERE Title LIKE 'WALL-%'
