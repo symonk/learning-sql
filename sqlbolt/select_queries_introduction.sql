f0bed660dc052137547073e19e2616d9104160a3
symonk <jackofspaces@gmail.com>
2023-08-28 14:18:30 +0100

    `sqlbolt:` chapter 1

---
+++ sqlbolt/select_queries_introduction.sql
@@ -0,0 +1,14 @@
+/* Exercise 1 */
+SELECT Title FROM Movies;
+
+/* Exercise 2*/
+SELECT Director FROM Movies;
+
+/* Exercise 3*/
+SELECT Title,Director FROM Movies;
+
+/* Exercise 4*/
+SELECT Title, Year FROM Movies;
+
+/* Exercise 5*/
+SELECT * FROM Movies;
