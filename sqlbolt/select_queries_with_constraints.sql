f4eb6120dd2c32b1f716810f5083634d03fc1a79
symonk <jackofspaces@gmail.com>
2023-08-28 14:24:48 +0100

    `select queries with constraints`

---
+++ sqlbolt/select_queries_with_constraints.sql
@@ -0,0 +1,12 @@
+
+/* Exercise 1 */
+SELECT * From Movies WHERE Id == 6;
+
+/* Exercise 2 */
+SELECT * FROM Movies Where Year BETWEEN 2000 AND 2010;
+
+/* Exercise 3 */
+SELECT * FROM Movies WHERE Year NOT BETWEEN 2000 AND 2010;
+
+/* Exercise 4 */
+SELECT * FROM Movies ORDER BY Year LIMIT 5;
